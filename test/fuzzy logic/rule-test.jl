a5d1b219ef938db8b13b3b7e744f3f8fb87c44b0
Donald Seinen <2869830202@qq.com>
2023-01-15 02:45:23 +0800

    rule API unit tests

--- test/fuzzy logic/rule-test.jl
+++ test/fuzzy logic/rule-test.jl
@@ -1,18 +1,21 @@
 using FuzzySystems.FuzzyLogic: @var, @rule, @rules, Rule
 using FuzzySystems.FuzzyLogic: Trapezoid, Triangular, nilpotent_minimum
 
-@testset "macros" begin
-    @testset "@var" begin
-        @test (@var {x: Q 0 0 2 4}) == Dict(:x => Trapezoid(0, 0, 2, 4))
-        # mixed float and ints in expr
-        @test (@var {x: Q 0.3 0 2 4}) == Dict(:x => Trapezoid(0.3, 0, 2, 4))
+want(op = nothing) = isnothing(op) ? Rule((:b, :c), :a) : Rule((:b, :c), :a, op)
+@testset "Rule & macros" begin
 
+    @testset "Rule" begin
+
+        @test want("MAX")   == want(maximum)
+        @test want(:MAX)    == want(maximum)
+        @test want(:min)    == want()
     end
+
     @testset "@rule" begin
-        @test (@rule a v1 = b v2 & c v3) == Rule((:b, :c), :a)
-        @test (@rule a = b & c) == Rule((:b, :c), :a)
-        @test Rule((:b,), :a, :MAX) == Rule((:b,), :a, maximum)
-        @test (@rule [MIN] a = b & c) == Rule((:b, :c), :a, minimum)
+        @test want() == @rule       a v1 = b v2 & c v3
+        @test want() == @rule       a    = b    & c
+        @test want() == @rule [MIN] a    = b    & c
+
     end
 
     @testset "@rules" begin
@@ -20,16 +23,28 @@ using FuzzySystems.FuzzyLogic: Trapezoid, Triangular, nilpotent_minimum
             a v1 = b v2 & c v3
             aa v1 = bb v2
         }
-        x₂ = @rules {a v1 = b v2 & c v3; aa v1 = bb v2}
-        x₃ = @rules {a = b & c; aa = bb}
+        x₂ = @rules {      a v1 = b v2 & c v3;       aa v1 = bb v2 }
+        x₃ = @rules {      a    = b    & c;          aa    = bb    }
+        x₄ = @rules {[min] a    = b    & c; [nilmin] aa    = b | d }
         y = ((@rule a v1 = b service & c food), (@rule aa v1 = bb service),)
-        z = (Rule((:b, :c), :a), Rule((:bb,), :aa))
+        z = (want(), Rule((:bb,), :aa))
         @test x₁ == x₂ == x₃
         @test x₁ == y
         @test y == z
-
-        x₄ = @rules {[min] a = b & c; [nilmin] aa = b | d}
-        @test x₄[1] == Rule((:b, :c), :a, minimum)
+        @test x₄[1] == want()
         @test x₄[2] == Rule((:b, :d), :aa, nilpotent_minimum)
+
+        @test (@rules {
+            {min} x = y
+            [min] xx = yy
+            [ min ] xxx = yyy
+        }) == (Rule((:y,), :x), Rule((:yy, ), :xx), Rule((:yyy,), :xxx))
     end
+
+    @testset "@var" begin
+        @test (@var {x: Q 0 0 2 4}) == Dict(:x => Trapezoid(0, 0, 2, 4))
+        @test (@var {x: Q .3 0 2 4}) == Dict(:x => Trapezoid(.3, 0, 2, 4))
+
+    end
+
 end
