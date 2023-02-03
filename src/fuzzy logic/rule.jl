8ea7b93eec2015e778c52418aaddc3befddae06b
Donald Seinen <2869830202@qq.com>
2023-02-03 17:36:15 +0800

    rule macro hygiene

--- src/fuzzy logic/rule.jl
+++ src/fuzzy logic/rule.jl
@@ -22,64 +22,93 @@ struct Rule
     out::Symbol
     op::Function
 end
-Rule(in::Tuple{Vararg{Symbol}}, out::Symbol, op::Union{Symbol, String}) = Rule(in, out, OP_ALIAS[Symbol(op)])
+function Rule(in::Tuple{Vararg{Symbol}}, out::Symbol, op::Union{Symbol, String})
+    Rule(in, out, OP_ALIAS[Symbol(op)])
+end
 Rule(in::Tuple{Vararg{Symbol}}, out::Symbol) = Rule(in, out, Base.minimum)
 Base.length(collection::Rule) = (length(collection.in), 1)
 
 """
-    @rule(expr...)
+@rule(expr...)
 Create a fuzzy [`Rule`](@ref).
 
 # Examples
 ```jldoctest
 julia> @rule cheap tip = poor service & bad food
 Rule((:poor, :bad), :cheap, "MAX")
+
+julia> @rule [bounded_sum] cheap = poor & bad
+Rule((:poor, :bad), :cheap, "MAX")
+
+julia> @rules {
+cheap = poor & bad
+average = good
+}
 ```
 """
-macro rule(expr...)
-    if expr[1] isa Expr && expr[1].head != :(=)
-        op = OP_ALIAS[expr[1].args[1]]
-        expr = expr[2:end]
-    end
-    make_symbol(string) = @eval Symbol(split($string, " ")[1])
-
-    str = collapse(string.(expr))
-    str = strip.(split(str, ('=', '|', '&')))
-
-    consequent = make_symbol(str[1])
-    popfirst!(str)
-    antecedent = ntuple(i -> make_symbol(str[i]), length(str))
-    if @isdefined op
-        Rule(antecedent, consequent, op)
-    else
-        Rule(antecedent, consequent)
-    end
-end
-
+macro rule end
+macro rule(ex) _rule(ex) end
+macro rule(ex...) _rule(ex...) end
 """
-    @rules(exprs...)
-Prepare multiple fuzzy rules of the form *Consequent = Antecedent₁ [&, |] Antecedent₂ ...*
+@rule(expr...)
+Create a fuzzy [`Rule`](@ref).
 
 # Examples
 ```jldoctest
+julia> @rule cheap tip = poor service & bad food
+Rule((:poor, :bad), :cheap, "MAX")
+
+julia> @rule [bounded_sum] cheap = poor & bad
+Rule((:poor, :bad), :cheap, "MAX")
+
 julia> @rules {
-    cheap tip = poor service & bad food
-    average tip = good service
-}
-# shorthand form
-julia> @rules {
-    cheap = poor & bad
-    average = good
+cheap = poor & bad
+average = good
 }
 ```
 """
-macro rules(exprs...)
-    str = collapse(string.(exprs))
-    str = match(r"(?<={).*(?=})", str).match
-    str = split(str, (';'))
+macro rules(ex) @inbounds Tuple(_rule(arg) for arg in ⋆ex) end
 
-    rules = ntuple(i -> Meta.parse("@rule " * str[i]), length(str))
-    esc(:(eval.($rules)))
+walk(expr::Expr) = expr |> walk!([]) |> Tuple
+walk(expr::Symbol) = (expr,)
+walk!(list) = ex -> begin
+    ex isa Symbol && return push!(list, ex)
+    ex isa Expr && map(walk!(list), @inbounds ex.args[2:end])
+    list
+end
+
+⋆(obj::Expr) = obj.args;
+⋆(obj::Expr, x::Int64)        = obj.args[x];
+⋆(obj::Expr, x::Tuple{Int64}) = obj.args[x[1]:end];
+
+function _rule(ex::Expr; op = minimum)
+    if ex ⋆ 1 isa Expr             # [op] (a | ) b = ...
+        op = OP_ALIAS[ex⋆1⋆1]
+        ex ⋆ 2 isa Expr ? ex = ex ⋆ 2 : popfirst!(⋆ex)
+    end
+    if ex.head != :(=)              # a b = c d | e f
+        out = ex ⋆ 1
+        in = (ex⋆2⋆2, (i ⋆ 3 for i in ex ⋆ (3,) if i isa Expr)...)
+    else                            # a = b & (c | d)
+        out = ex ⋆ 1
+        in = walk(ex ⋆ 2)
+    end
+    Rule(in, out, op)
+end;
+
+function _rule(ex...)
+    op = minimum
+    if ex[1] isa Expr
+        op = OP_ALIAS[ex[1] ⋆ 1]
+        if ex[2] isa Expr
+            return _rule(ex[2], op = op) # [op] a = b & c
+        else
+            ex = ex[2:end] # [op] a b = c d & e f
+        end
+    end
+    out = ex[1]
+    in = Tuple((i.head == :(=) ? i ⋆ 2 : i ⋆ 3 for i in ex[2:end-1]))
+    Rule(in, out, op)
 end
 
 """
@@ -92,28 +121,10 @@ julia> @var {
     poor     : Q 0 0 2 4
     good     : T 3 5 7
 }
-Dict{Symbol, MF} with 3 entries:
+Dict{Symbol, MF} with 2 entries:
     :excellent => Q|₆₌⁸⁼¹⁼₁₌|
     :good => T|₃₌⁵⁼₇₌|
 ```
 """
-macro var(exprs...)
-    str = collapse(string.(exprs))
-    str = match(r"(?<={).*(?=})", str).match
-    str = split(str, (';', ':'))
-    keys, values = [], []
-    for (i, j) in Iterators.partition(str, 2)
-        # check if an (unexported) alias was used
-        fun = MF_ALIAS[Symbol(match(r"^[^ ]*", j).match)]
-        var_name = Symbol(:($(lstrip(i))))
-        push!(keys, var_name)
-        args = eval(
-            Meta.parse(
-                "(" * replace(match(r"(?<= ).*", j).match, " " => ",") * ")"
-            )
-        )
-        args = Tuple(Float64(x) for x in args) # catch any integers
-        push!(values, :($(fun(args...))))
-    end
-    Dict{Symbol, MF}(zip(keys, values))
-end
+macro var(ex) _var(ex) end;
+_var(ex) = Dict{Symbol, MF}(x⋆1⋆2 => MF_ALIAS[x⋆1⋆3](x ⋆ (2,)...) for x in ⋆ex);
