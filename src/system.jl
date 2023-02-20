43652dc2f7f4653538c7b77405d2be0a6b4da10d
Donald Seinen <2869830202@qq.com>
2023-02-20 22:07:17 +0800

    remove comments

--- src/system.jl
+++ src/system.jl
@@ -20,17 +20,7 @@ function predict(fis::Mamdani, data, defuzz = WTAV)
     strength = Float64[]
     @inbounds @views for rule in fis.rules
         tmp_s = ntuple(i -> μ(data[i], fis.input_dict[i][rule.in[i]]), length(rule.in))
-        #push!(strength, firing(tmp_s, rule.op))
         push!(strength, rule.op(tmp_s))
     end
     defuzz(strength, fis)
 end
-
-#= function firing(tmp_strengths::Tuple{Vararg{Float64}}, firing_method::AbstractString)
-    if firing_method == "MIN"
-        return	minimum(tmp_strengths)
-    elseif firing_method == "MAX"
-        return	maximum(tmp_strengths)
-    end
-end
- =#
